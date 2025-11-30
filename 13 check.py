de6cbd042e7ea94ebbb885e7c869c4e78110a38c
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:52:15 +0500

    Add files via upload

---
+++ 13 check.py
@@ -0,0 +1,5 @@
+meals = {"Burger": 550,"Pizza": 700,"Pasta": 480,"Biryani": 650,"Shawarma": 450}
+meals["Sandwich"] = 300   # add new meal item
+print(meals)
+
+answer= {'Burger': 550, 'Pizza': 700, 'Pasta': 480, 'Biryani': 650, 'Shawarma': 450, 'Sandwich': 300}
