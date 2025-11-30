8707bb8c56df29f292ebd44eb3e8eee94812cccf
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:27:49 +0500

    Add files via upload

---
+++ 1 check.py
@@ -0,0 +1,7 @@
+books = {"Harry Potter": "J.K. Rowling","The Hobbit": "J.R.R. Tolkien","The Alchemist": "Paulo Coelho","1984": "George Orwell","Pride and Prejudice": "Jane Austen"}
+
+print(books["Harry Potter"])
+print(books["The Hobbit"])
+print(books["The Alchemist"])
+print(books["1984"])
+print(books["Pride and Prejudice"])
