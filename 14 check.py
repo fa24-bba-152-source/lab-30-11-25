734f83f16773164b6669a786e424d491583d5098
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:57:09 +0500

    Add files via upload

---
+++ 14 check.py
@@ -0,0 +1,7 @@
+countries = {"Pakistan": "Rupee", "USA": "Dollar","China": "Yuan", "India": "Rupee", "Japan": "Yen"}
+if "India" in countries:
+    print("India is included")
+else:
+    print("India is not included")
+
+answer =India is included
---
+++ 15 check.py
@@ -0,0 +1,13 @@
+friends = {"Ali": "10 March","Sara": "22 May","Hamza": "5 July","Zara": "14 October","Umer": "31 December"}
+print(friends["Ali"])
+print(friends["Sara"])
+print(friends["Hamza"])
+print(friends["Zara"])
+print(friends["Umer"])
+
+answer=
+10 March
+22 May
+5 July
+14 October
+31 December
