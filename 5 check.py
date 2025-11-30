f88ce9ef443aba9c083d5cd5be30bdb37e474a68
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:39:45 +0500

    Add files via upload

---
+++ 5 check.py
@@ -0,0 +1,14 @@
+animals = {"Dog": "Bark","Cat": "Meow","Cow": "Moo","Lion": "Roar","Duck": "Quack"}
+
+print(animals["Dog"])
+print(animals["Cat"])
+print(animals["Cow"])
+print(animals["Lion"])
+print(animals["Duck"])
+
+answer
+Bark
+Meow
+Moo
+Roar
+Quack
