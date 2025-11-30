d4683268cd0d4a3c9002b1afd83f5906ec85270b
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:38:37 +0500

    Add files via upload

---
+++ 4 check.py
@@ -0,0 +1,8 @@
+subjects = {"Math": 90, "English": 85, "Science": 88, "Computer": 92, "Urdu": 80}
+
+if "Math" in subjects:
+    print("Math is present")
+else:
+    print("Math is not present")
+
+answer = "Math" in subjects
