6524be9ea67b99eae8103d604dbe564cf298ff66
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:36:35 +0500

    Add files via upload

---
+++ 3 check.py
@@ -0,0 +1,14 @@
+movies = {"Avatar": 2009,"Titanic": 1997,"Inception": 2010,"Joker": 2019,"Interstellar": 2014}
+
+print(movies["Avatar"])
+print(movies["Titanic"])
+print(movies["Inception"])
+print(movies["Joker"])
+print(movies["Interstellar"])
+
+answer
+2009
+1997
+2010
+2019
+2014
