2e2cfba51ac847312761d33243660630612ee585
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:48:16 +0500

    Add files via upload

---
+++ 10 check.py
@@ -0,0 +1,14 @@
+players = {"Babar": 88, "Rizwan": 75, "Shaheen": 40, "Fakhar": 60, "Nawaz": 32}
+
+print(players["Babar"])
+print(players["Rizwan"])
+print(players["Shaheen"])
+print(players["Fakhar"])
+print(players["Nawaz"])
+
+answer =
+88
+75
+40
+60
+32
