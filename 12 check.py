aa5d934aefc8b0e6baf0481961df9240f2d0fdcf
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:50:24 +0500

    Add files via upload

---
+++ 12 check.py
@@ -0,0 +1,6 @@
+cars = {"Civic": "Honda", "Corolla": "Toyota", "Sportage": "Kia", "City": "Honda", "Vitz": "Toyota"}
+cars["Vitz"] = "Suzuki"   # update company
+
+print(cars)
+
+answer= {'Civic': 'Honda', 'Corolla': 'Toyota', 'Sportage': 'Kia', 'City': 'Honda', 'Vitz': 'Suzuki'}
