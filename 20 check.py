71d7a781dfe02b9d269831bf8a678970b67bcfc2
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 15:05:34 +0500

    Add files via upload

---
+++ 20 check.py
@@ -0,0 +1,5 @@
+teachers = {  "Sir Ali": "Math",   "Miss Sara": "English",   "Sir Hamza": "Science",   "Miss Zara": "Computer",   "Sir Ahmed": "Physics"}
+teachers["Miss Sara"] = "Biology"   c
+print(teachers)
+
+# Output: {'Sir Ali': 'Math', 'Miss Sara': 'Biology', 'Sir Hamza': 'Science', 'Miss Zara': 'Computer', 'Sir Ahmed': 'Physics'}
---
+++ 21 check.py
@@ -0,0 +1,8 @@
+mobiles = {  "Samsung": "Galaxy S24",  "Apple": "iPhone 15",  "Xiaomi": "Mi 14",  "Oppo": "Find X7",  "Vivo": "X100"}
+if "Apple" in mobiles:
+    print("Brand exists")
+else:
+    print("Brand does not exist")   
+
+
+# Output: Brand exists
