1b0e9328ab237274becfd9833b246a31cf9c870e
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:46:55 +0500

    Add files via upload

---
+++ 9 check.py
@@ -0,0 +1,6 @@
+laptops = {"HP": "Pavilion 15", "Dell": "Inspiron 5502", "Lenovo": "ThinkPad X1"}
+laptops["Dell"] = "Inspiron 5510"   # modify model number
+
+print(laptops)
+
+answer= {'HP': 'Pavilion 15', 'Dell': 'Inspiron 5510', 'Lenovo': 'ThinkPad X1'}
