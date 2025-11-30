0db7d11697a13536400d265b75db1fd3387c4f31
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:44:04 +0500

    Add files via upload

---
+++ 7 check.py
@@ -0,0 +1,6 @@
+fruits = {"Apple": 200, "Banana": 100, "Mango": 250, "Orange": 180, "Grapes": 300}
+fruits["Pineapple"] = 350   # add new fruit
+
+print(fruits)
+ 
+ answer {'Apple': 200, 'Banana': 100, 'Mango': 250, 'Orange': 180, 'Grapes': 300, 'Pineapple': 350}
