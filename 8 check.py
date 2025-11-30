66282588cdc98592c3042e619a9878280f78d13e
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:45:25 +0500

    Add files via upload

---
+++ 8 check.py
@@ -0,0 +1,8 @@
+apps = {"WhatsApp": 4.5, "Instagram": 4.4, "Facebook": 3.9, "TikTok": 4.4, "Snapchat": 4.2}
+
+if "WhatsApp" in apps:
+    print("WhatsApp found")
+else:
+    print("WhatsApp not found")
+
+answer= WhatsApp found
