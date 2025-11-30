39117b5e0cf2695f79aeed1aebc2336718b653e1
fa24-bba-152-source <fa24-bba-152@cuilahore.edu.pk>
2025-11-30 14:49:33 +0500

    Add files via upload

---
+++ 11 check.py
@@ -0,0 +1,13 @@
+words = {"Brave": "Fearless","Calm": "Peaceful","Rapid": "Fast","Tiny": "Very small", "Huge": "Very big"}
+print(words["Brave"])
+print(words["Calm"])
+print(words["Rapid"])
+print(words["Tiny"])
+print(words["Huge"])
+
+answer= 
+Fearless
+Peaceful
+Fast
+Very small
+Very big
